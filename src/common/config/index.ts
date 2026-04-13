15ee6ba9a56e02a96b27f6f43ba2fd856fbb1489
Abel Bogale (pyankie) <zpyankie@gmail.com>
2026-04-13 13:14:34 +0300

    chore(config): restore config barrel exports

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -1,1 +1,4 @@
 export { default as appConfig } from './app.config';
+export { validateEnv } from './validation-schema';
+export type { EnvironmentVariables } from './validation-schema';
+export { setupSwagger } from './swagger.setup';
