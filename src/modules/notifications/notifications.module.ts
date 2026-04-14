ebd780c64953e1a27ac5a7b829bcce858280b39a
Abel Bogale (pyankie) <zpyankie@gmail.com>
2026-04-14 22:51:27 +0300

    feat(notifications): add firebase push provider

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import { PushProvider } from './providers/push.provider';
 import { SmsProvider } from './providers/sms.provider';
 
 @Module({
-  providers: [NotificationsService, SmsProvider],
+  providers: [NotificationsService, SmsProvider, PushProvider],
   exports: [NotificationsService],
 })
 export class NotificationsModule {}
