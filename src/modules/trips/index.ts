5a958ea79ab34de133a11ab6ba3481be3d0dd7dc
Abel Bogale (pyankie) <zpyankie@gmail.com>
2026-04-30 16:30:09 +0300

    feat(trips): wire trips files into trips module

---
+++ src/modules/trips/index.ts
@@ -0,0 +1,4 @@
+export { TripsModule } from './trips.module';
+export { TripsService } from './trips.service';
+export { CreateTripDto } from './dto/create-trip.dto';
+export { TripQueryDto } from './dto/trip-query.dto';
