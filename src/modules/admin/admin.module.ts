965d3258989d9c2f8ea810cba8476e66301a4fba
Abel Bogale (pyankie) <zpyankie@gmail.com>
2026-05-06 22:38:14 +0300

    feat(admin): wire admin module

---
+++ src/modules/admin/admin.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { RoutesModule } from '../routes/routes.module';
+import { TripsModule } from '../trips/trips.module';
+import { UsersModule } from '../users/users.module';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+
+@Module({
+  imports: [UsersModule, RoutesModule, TripsModule, NotificationsModule],
+  controllers: [AdminController],
+  providers: [AdminService],
+})
+export class AdminModule {}
