d2ddfa824759fa3b36012ec7f7636f517dcc8acb
Abel Bogale (pyankie) <zpyankie@gmail.com>
2026-05-20 02:31:45 +0300

    feat(tickets): wire notifications module into tickets module

--- src/modules/tickets/tickets.module.ts
+++ src/modules/tickets/tickets.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from '../../common/common.module';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 import { RoutesModule } from '../routes/routes.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { TicketsController } from './tickets.controller';
@@ -8,7 +9,7 @@ import { TicketsService } from './tickets.service';
 import { QrService } from './qr.service';
 
 @Module({
-  imports: [PrismaModule, CommonModule, WalletModule, RoutesModule],
+  imports: [PrismaModule, CommonModule, WalletModule, RoutesModule, NotificationsModule],
   controllers: [TicketsController],
   providers: [TicketsService, QrService],
   exports: [TicketsService, QrService],
